1d74add875d2354ec7d7bb80097c2eb6243e6932
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2025-03-20 11:17:26 +0100

    Update PartialConstructors.cs

--- PartialConstructors.cs
+++ PartialConstructors.cs
@@ -9,7 +9,7 @@ namespace CsharpVersion14Samples
     public partial class Employee
     {
         // Defining declaration of the partial constructor
-        public partial Employee(string firstName, string lastName);
+        //public partial Employee(string firstName, string lastName);
 
         public string FullName { get; private set; }
     }
@@ -17,11 +17,11 @@ namespace CsharpVersion14Samples
     public partial class Employee
     {
         // Implementing declaration of the partial constructor
-        public partial Employee(string firstName, string lastName) : this() // Constructor initializer goes here
-    {
-            FullName = $"{firstName} {lastName}";
-            Console.WriteLine($"Employee partial constructor invoked: {FullName}");
-        }
+        //public partial Employee(string firstName, string lastName) : this() // Constructor initializer goes here
+        //{
+        //    FullName = $"{firstName} {lastName}";
+        //    Console.WriteLine($"Employee partial constructor invoked: {FullName}");
+        //}
 
         //// Base parameterless constructor
         public Employee()
